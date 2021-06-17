443ef5cb20d4592db3b01a97fd277e6bde1bd53f
dlkennen <dlkennen@comcast.net>
2021-06-17 18:24:47 -0400

    Updates to app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,36 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-control");
+
+// Create event handlers 
+form.on("submit",runEnter);
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(ufoentry => ufoentry.datetime === inputValue);
+
+    console.log(filteredData);};
+
+    var tbody = d3.select("tbody");
+
+    filteredData.forEach((ufotable) => {
+        var row = tbody.append("tr");
+        Object.entries(ufotable).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+
+    //tableData.html("")
