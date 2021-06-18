555428c5102c4cad44683a746dcb124a9e8bc8a3
dlkennen <dlkennen@comcast.net>
2021-06-18 11:07:52 -0400

    Finished app file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,9 @@
+//Javascript HW - Diana Kennen
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
-// Select the button
+// Select the button and input form
 var button = d3.select("#filter-btn");
 var form = d3.select(".form-control");
 
@@ -10,28 +11,55 @@ var form = d3.select(".form-control");
 form.on("submit",runEnter);
 button.on("click", runEnter);
 
+//Select the table body
+var tbody = d3.select("tbody");
+console.log(tableData)
+
+//Displaying unfiltered data table
+tableData.forEach((ufotable) => {
+  var row = tbody.append("tr");
+  Object.entries(ufotable).forEach(([key, value]) => {
+  var cell = row.append("td");
+  cell.text(value);
+  });
+});
+
 // Complete the event handler function for the form
 function runEnter() {
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
+  //Emptying table
+  tbody.html("");
 
-    var filteredData = tableData.filter(ufoentry => ufoentry.datetime === inputValue);
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-    console.log(filteredData);};
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
 
-    var tbody = d3.select("tbody");
+  //If then conditional to reset with the complete table or use a filtering date.
+  //If the input field is blank or deleted, use the full table data to complete the table.
+  if (inputValue === "") {
+    tableData.forEach((ufotable) => {
+      var row = tbody.append("tr");
+      Object.entries(ufotable).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+      });
+    }); 
+  //If the input field contains a date, create a new filtered data array for completing the table. 
+  } else {
+      var filteredData = tableData.filter(ufoentry => ufoentry.datetime === inputValue);
 
-    filteredData.forEach((ufotable) => {
-        var row = tbody.append("tr");
-        Object.entries(ufotable).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-    });
+      console.log(filteredData);
 
-    //tableData.html("")
+      filteredData.forEach((ufotable) => {
+      var row = tbody.append("tr");
+      Object.entries(ufotable).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+      });
+    });}}
